d49d7a12393b19d9be6454164b4f06398ad27d05
Nick <getkan@local.attlocal.net>
2026-04-29 22:35:32 -0500

    Configure SvelteKit static build for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,17 +2,17 @@ import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		runes: ({ filename }) =>
-			filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
-	},
-	kit: {
-		adapter: adapter({
-			pages: "build",
-			assets: "build",
-			fallback: "404.html",
-		}),
-	},
+    compilerOptions: {
+        runes: ({ filename }) =>
+            filename.split(/[/\\]/).includes("node_modules") ? undefined : true
+    },
+    kit: {
+        adapter: adapter({
+            pages: "build",
+            assets: "build",
+            fallback: "404.html"
+        })
+    }
 };
 
 export default config;
